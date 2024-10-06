27ed7cb71b0eef742d3e3f496fc3a7e50addfb4d
PC\eyerj <joel.eyer@bwz-rappi.ch>
2024-10-06 20:12:09 +0200

    New class added with the name FileHandler

---
+++ AutomaticScanning/FileHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticScanning
+{
+    internal class FileHandler
+    {
+        public static string UserFolderPath = AppContext.BaseDirectory + @"User\";
+
+        public static string UserScannerSettingsPath = UserFolderPath + "ScannerSettings.json";
+        public static UserScannerSettings UserScannerSettings;
+
+        public static string UserSaveSettingsPath = UserFolderPath + "SaveSettings.json";
+        public static UserSaveSettings UserSaveSettings;
+
+        public void StartupProgram()
+        {
+            UserScannerSettings = ReadScannerSettingsJson();
+            UserSaveSettings = ReadSaveSettingsJson();
+        }
+
+        public UserScannerSettings ReadScannerSettingsJson()
+        {
+
+        }
+
+        public void WriteScannerSettingsJson()
+        {
+
+        }
+
+        public UserSaveSettings ReadSaveSettingsJson()
+        {
+
+        }
+
+        public void WriteSaveSettingsJson()
+        {
+
+        }
+    }
+
+    class UserScannerSettings
+    {
+
+    }
+
+    class UserSaveSettings
+    {
+
+    }
+}
